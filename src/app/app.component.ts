b4457fefb6760e816aa2b69d2f78e8c9f062de5b
Daniel Sá Barretto <dan.barretto98@gmail.com>
2024-05-09 22:35:24 -0300

    chore(config): Config material design 3 theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCheckbox } from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCheckbox],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
