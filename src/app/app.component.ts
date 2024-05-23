53c24e10de98fe68dd546f91d0c79a05bb2d818a
Daniel Sá Barretto <dan.barretto98@gmail.com>
2024-05-23 14:47:03 -0300

    fix(style): Add custom styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material.module';
+import { Component } from '@angular/core';
 
 
 @Component({
