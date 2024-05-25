f3aeee6b0d8d2b95374bbedcda294347f4af3e4d
Tiago Marino <tmsilva11@gmail.com>
2024-05-25 20:35:22 -0300

    Add custom password validator

--- src/services/createAccount.service.ts
+++ src/services/createAccount.service.ts
@@ -1,15 +1,56 @@
 import { Injectable } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, ValidationErrors, Validators } from '@angular/forms';
+
+class CustomValidators {
+  /**
+   * @description Validator for password.
+   *   
+   * @returns An error map that contains one of the properties listed below or null if it is valid:
+   * - minLength  : at least 10 characters
+   * - lowerCase  : at least one lowercase character
+   * - upperCase  : at least one uppercase character
+   * - digit      : at least one digit
+   * - specialChar: at least one special character (!@#$%&*)
+   */
+  static password(control: AbstractControl<string>): ValidationErrors | null {
+    const value = control.value
+    const minLength = 10;
+    const lowerCaseRegex = /^(?=.*[a-z])/;
+    const upperCaseRegex = /^(?=.*[A-Z])/;
+    const digitRegex = /^(?=.*\d)/;
+    const specialCharRegex = /^(?=.*[!@#$%&*])/;
+
+    const validationErrors: ValidationErrors = {};
+    if (!value) {
+      return null;
+    }
+    if (value.length < minLength) {
+      validationErrors["minLength"] = { minLength: minLength, actual: value.length } 
+    }
+    if (!lowerCaseRegex.test(value)) {
+      validationErrors["lowerCase"] = { lowerCase: false }
+    }
+    if (!upperCaseRegex.test(value)) {
+      validationErrors["upperCase"] = { upperCase: false }
+    }
+    if (!digitRegex.test(value)) {
+      validationErrors["digit"] = { digit: false }
+    }
+    if (!specialCharRegex.test(value)) {
+      validationErrors["specialChar"] = { specialChar: false }
+    }
+
+    return Object.is(validationErrors, {}) ? null : validationErrors;
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateAccountService {
-
-  private passwordRegex: RegExp = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%&?]).{10,}$/;
   
   private readonly _emailFormControl: FormControl = new FormControl('', [Validators.required, Validators.email]);
-  private readonly _passwordFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(this.passwordRegex)]);
+  private readonly _passwordFormControl: FormControl = new FormControl('', [Validators.required, CustomValidators.password]);
   
   public get emailFormControl(): FormControl {
     return this._emailFormControl;
@@ -18,8 +59,6 @@ export class CreateAccountService {
   public get passwordFormControl(): FormControl {
     return this._passwordFormControl;
   }
-  
-  constructor() { }
 
   isEqual(field1: string, field2: string): boolean {
     return field1 === field2;
