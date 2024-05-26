7d020dbc084db981239da35f79906c5dae20040e
Breno Pejon <brepejon@gmail.com>
2024-05-26 12:49:24 -0300

    wip: footer

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../app/material.module';
 import { TopAppBarComponent } from '../../components/top-app-bar/top-app-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [MaterialModule, TopAppBarComponent],
+  imports: [MaterialModule, TopAppBarComponent,FooterComponent],
   standalone: true
 })
 export class HomeComponent implements OnInit {
