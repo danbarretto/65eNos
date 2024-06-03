3e261f52b75f868daf46197db8e825ab254ff988
danbarretto <dan.barretto98@gmail.com>
2024-06-03 12:08:11 -0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from '../pages/home/home.component';
-import { NewsPageComponent } from '../pages/news-page/news-page.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'noticias/:id', component: NewsPageComponent }
-];
-
