d09c8a34498c2dd6c11e8c4a5b60fb11940440cf
Tyago Teoi <tyago.teoi@usp.br>
2024-06-06 04:21:24 -0300

    Add page footer to news pages

--- src/pages/news-page/news-page.component.ts
+++ src/pages/news-page/news-page.component.ts
@@ -3,6 +3,7 @@ import { MaterialModule } from '../../app/material.module';
 import { ActivatedRoute } from '@angular/router';
 import { NewsArticle, NewsService } from '../../services/news.service';
 import { TopAppBarComponent } from '../../components/top-app-bar/top-app-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 import { ViewportScroller } from '@angular/common';
 import { ScrollTopButtonComponent } from '../../components/scroll-top-button/scroll-top-button.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,7 +16,7 @@ import { FontSizeService } from '../../services/font-size.service';
   templateUrl: './news-page.component.html',
   styleUrls: ['./news-page.component.scss'],
   standalone: true,
-  imports: [MaterialModule, TopAppBarComponent, ScrollTopButtonComponent, HttpClientModule, CommentSectionComponent]
+  imports: [MaterialModule, TopAppBarComponent, FooterComponent, ScrollTopButtonComponent, HttpClientModule, CommentSectionComponent]
 })
 export class NewsPageComponent implements OnInit {
 
