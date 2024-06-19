c6e54c8a90e4bdfc80fbc57638ccfc0c739f5a18
Tiago Marino <tmsilva11@gmail.com>
2024-06-18 23:06:33 -0300

    Add header and footer to login page

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MaterialModule } from '../../app/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
+import { TopAppBarComponent } from "../../components/top-app-bar/top-app-bar.component";
+import { OverlayMenuComponent } from "../../components/overlay-menu/overlay-menu.component";
+import { ToggleMenuService } from '../../services/toggle-menu.service';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ScrollTopButtonComponent } from "../../components/scroll-top-button/scroll-top-button.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  standalone: true,
-  imports: [MaterialModule, FormsModule, ReactiveFormsModule]
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    standalone: true,
+    imports: [MaterialModule, FormsModule, ReactiveFormsModule, TopAppBarComponent, OverlayMenuComponent, ScrollTopButtonComponent, FooterComponent]
 })
 export class LoginComponent {
 
+  @ViewChild('sidenav') sidenav: MatSidenav
 
   email: string = ''
   password: string = ''
   errorMessage: string = ''
   showPassword = false
 
-  constructor(private router: Router, private authService: AuthenticationService) {
-
+  constructor(private router: Router, private authService: AuthenticationService, private toggleMenu: ToggleMenuService) {
+    this.toggleMenu.toggle$.subscribe(() => {
+      this.sidenav.toggle()
+    })
   }
 
   togglePassword() {
